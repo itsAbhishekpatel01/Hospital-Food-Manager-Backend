bd7e9f0a1efb8828494d2aea4d9290fde9d771ca
Abhishek Patel <itsabhishekpatel01@gmail.com>
2025-01-13 11:15:20 +0530

    Patient controller written

--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -3,6 +3,13 @@ const Patient = require('../models/patient.model');
 const getAllPatients = async (req, res)=>{
     try {
         const patientList = await Patient.find();
+        if(patientList.length === 0){
+            return res.status(204).json({
+                        success: true,
+                        error: false,
+                        message : "No content"
+            })
+        }
         return res.status(200).json({
             success: true,
             error: false,
@@ -12,17 +19,18 @@ const getAllPatients = async (req, res)=>{
         return res.status(500).json({
             success: false,
             error: true,
-            error : error
+            message : error
         })
     }
 }
+
 
 const getPatientById = async (req, res) => {
     try {
         const {id} = req.params;
     const patient = await Patient.findOne({patientId:id});
     if(!patient){
-        return res.status(200).json({
+        return res.status(404).json({
             success: false,
             error: true,
             message:"No patient found with this id"
@@ -37,7 +45,7 @@ const getPatientById = async (req, res) => {
         return res.status(500).json({
             success: false,
             error: true,
-            error : error
+            message : error
         })
     }
 }
@@ -46,42 +54,67 @@ const createPatient = async (req, res) => {
         try {
             const newPatient = req.body;
             if(!newPatient.patientId || !newPatient.name || !newPatient.roomNumber || !newPatient.bedNumber || !newPatient.floorNumber || !newPatient.age || !newPatient.gender || !newPatient.contactInformation || !newPatient.emergencyContact){
-                return res.status(400).send('Missing required fields')
+                return res.status(500).json({
+                            success: false,
+                            error: true,
+                            message : 'Missing required fields'
+                })
             }
             //check by patientId if patient already exists
             const patient = await Patient.findOne({patientId: newPatient.patientId});
             if(patient){
-                return res.status(400).send('Patient already exists')
+                return res.status(500).json({
+                            success: false,
+                            error: true,
+                            message : 'Patient already exists'
+                })
             }
             
             // check by bedNumber if bed is already occupied
             const bedOccupied = await Patient.findOne ({bedNumber: newPatient.bedNumber, roomNumber: newPatient.roomNumber});
             if(bedOccupied){
-                return res.status(400).send('Bed already occupied')
+                return res.status(500).json({
+                            success: false,
+                            error: true,
+                            message : 'Bed already occupied'
+                })
             }
 
             const resp =  await Patient.create(newPatient);
             return res.status(201).send({data: resp});
         } catch (error) {
-            res.status(500).send('Error creating patient')
+            return res.status(500).json({
+                        success: false,
+                        error: true,
+                        message : 'Error creating patient'
+            })
         }
 }
 
 const updatePatient = async (req, res) => {
     try {
         const {id} = req.params;
-    const patient = await Patient.findOne({patientId:id});
-    if(!patient){
+        const updateData = req.body;
+        if(!id || !updateData){
+            return res.status(400).json({
+                        success: false,
+                        error: true,
+                        message : 'Invalid request data'
+            })
+        }
+        const updatedPatient = await Patient.findOneAndUpdate({patientId:id}, updateData, {new:true});
+        if(!updatedPatient){
+            return res.status(500).json({
+                        success: false,
+                        error: true,
+                        message : "No patient found with this patientId"
+            })
+        }
+        
         return res.status(200).json({
-            success: false,
-            error: true,
-            message:"No patient found with this id"
-        })
-    } 
-    return res.status(200).json({
-        success: true,
-        error: false,
-        patient : patient
+            success: true,
+            error: false,
+            updatedPatient : updatedPatient
     })        
     } catch (error) {
         return res.status(500).json({
@@ -93,8 +126,29 @@ const updatePatient = async (req, res) => {
 }
 
 const deletePatient = async (req, res) => {
-    const { id } = req.params;
-    res.send(`This will delete the patient with ID: ${id}`);
+    try {
+        const {id} = req.params;
+        const patient = await Patient.findOneAndDelete({patientId:id});
+        if(!patient){
+            return res.status(500).json({
+                        success: false,
+                        error: true,
+                        message : "No patient found with this patientId"
+            })
+        }
+        
+        return res.status(200).json({
+            success: true,
+            error: false,
+            message : "Patient deleted successfully"
+    })        
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: true,
+            error : error
+        })
+    }
 }
 
 module.exports = {getAllPatients, getPatientById, createPatient, updatePatient, deletePatient };
